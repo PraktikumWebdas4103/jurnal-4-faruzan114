77206a118b6d0871491f63c58b62250f9017c2fd
faruzan114 <43526521+faruzan114@users.noreply.github.com>
2018-09-25 16:54:10 +0700

    Add files via upload

--- formawal.php
+++ formawal.php
@@ -1,5 +1,6 @@
-<?php
+<form method="post" action="proses.php">
+<br><b>Username :</b><input name="Username" type="text" size="30" maxlength="100">
+<br><b>Password :</b><input name="password" type="password" size="30" maxlength="100">
+<br><input type="submit" name="login" value="Login">
+</form>
 
-
-
-?>
--- proses.php
+++ proses.php
@@ -1,4 +1,18 @@
-<?php
+<form action=" " method="POST">
+  <input type="checkbox" name="hobi[]" value="Olahraga">Olahraga<br />
+  <input type="checkbox" name="hobi[]" value="Memancing">Memancing<br />
+  <input type="checkbox" name="hobi[]" value="Membaca">Membaca<br />
+  <input type="checkbox" name="hobi[]" value="Travelling">Travelling<br />
+  <input type="submit" value="Kirim">
+</form>
 
+<?php
+$hobi = $_POST["hobi"];
+echo "<p>Hobi yang Anda pilih:</p>";
+foreach ($hobi as $nilai) {
+echo "- $nilai <br />";
+}
+
+echo "string";
 
 ?>
